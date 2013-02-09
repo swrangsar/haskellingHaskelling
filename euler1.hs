0ed69a4e5a0009478ff2ca53e06af94dca6a7698
Swrangsar Basumatary <swrangsar@aol.com>
2013-02-09 15:42:41 +0530

    sjfkdjkj

--- euler1.hs
+++ euler1.hs
@@ -1,10 +1,6 @@
--- multiples of 3 or 5 below 1000
-isMultipleOf3or5 :: Integer -> Bool
-isMultipleOf3or5 n = if (mod n 3 == 0 || mod n 5 == 0)
-                        then True
-                        else False
+-- sum of multiples of 3 or 5 below 1000
 
-answer = sum $ filter isMultipleOf3or5 $ takeWhile (< 1000) [1..]
+answer = sum $ filter (\x -> mod x 3 == 0 || mod x 5 == 0) $ takeWhile (< 1000) [1..]
 
 main = do
     putStrLn (show answer)
