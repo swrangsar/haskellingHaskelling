d7f2bf2d87e3c87397b83cb47b7b57483177d03a
Swrangsar Basumatary <swrangsar@aol.com>
2013-02-10 16:18:10 +0530

    skjflkds

--- euler2.hs
+++ euler2.hs
@@ -1,9 +1,9 @@
 -- sum of even fibonacci numbers not greater than 4 million
 fibs :: [Integer]
-fibs = 0:1:[a+b | (a,b) <- zip fibs (tail fibs)]
+fibs = 0:1:zipWith (+) fibs (tail fibs)
 
 result :: Integer
-result = sum $ takeWhile (<= 4000000) $ filter even fibs
+result = sum $ filter even $ takeWhile (<= 4000000) fibs
 
 main = do
     putStrLn "The sum of even fibonacci numbers is: "
